ec3988d978d825c3a444a80f1040c762b54a1913
Yuri Karabatov <yuri@yurikarabatov.com>
2024-02-10 21:07:16 +0000

    Layer formatting

--- keyboards/idobao/id75/keymaps/karabatov/keymap.c
+++ keyboards/idobao/id75/keymaps/karabatov/keymap.c
@@ -1,14 +1,39 @@
 #include QMK_KEYBOARD_H
 
+// Layers
+#define _LBASE 0
+#define _LSYMB 1
+#define _LGAME 2
+#define _LARRW 3
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_ortho_5x15(
-		KC_GRV, 			KC_1, 		KC_2, 		KC_3, 		KC_4, 			KC_5, 	KC_VOLD, 	KC_MUTE, 	KC_VOLU, 	KC_6, 	KC_7, 			KC_8, 			KC_9, 			KC_0, 		KC_DEL, 
-		KC_TAB, 			KC_Q, 		KC_W, 		KC_E, 		KC_R, 			KC_T, 	KC_MPRV, 	KC_MPLY, 	KC_MNXT, 	KC_Y, 	KC_U, 			KC_I,			KC_O, 			KC_P, 		KC_BSPC, 
-		LCTL_T(KC_ESC), 	KC_A, 		KC_S, 		KC_D, 		KC_F, 			KC_G, 	KC_NO, 		KC_NO, 		KC_F15, 	KC_H, 	KC_J, 			KC_K, 			KC_L, 			KC_SCLN, 	RCTL_T(KC_QUOT), 
-		KC_LSFT, 			KC_Z, 		KC_X, 		KC_C, 		KC_V, 			KC_B, 	KC_NO, 		KC_NO, 		KC_F14, 	KC_N, 	KC_M, 			KC_COMM, 		KC_DOT, 		KC_SLSH, 	SC_SENT, 
-		SGUI_T(KC_MUTE), 	KC_LCTL, 	KC_LALT, 	KC_LGUI, 	LT(2,KC_SPC), 	KC_SPC, KC_SPC, 	KC_NO, 		KC_SPC, 	KC_SPC, LT(1,KC_SPC), 	RCTL(KC_RSFT), 	RCTL(KC_RALT), 	KC_RALT, 	LT(3,KC_DOWN)),
-	[1] = LAYOUT_ortho_5x15(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_TRNS, KC_TRNS, KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_TRNS, KC_UNDS, KC_LPRN, KC_LBRC, KC_LCBR, KC_EQL, KC_TRNS, KC_TRNS, KC_TRNS, KC_PLUS, KC_RCBR, KC_RBRC, KC_RPRN, KC_COLN, KC_DQUO, KC_TRNS, KC_TRNS, KC_BSLS, KC_PIPE, KC_GRV, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_MINS, KC_LT, KC_GT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP, KC_PGDN, KC_END),
-	[2] = LAYOUT_ortho_5x15(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCRL, KC_TRNS, KC_TRNS, KC_WH_U, KC_MS_U, KC_WH_D, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT, KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN2, KC_BTN1, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-	[3] = LAYOUT_ortho_5x15(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO)
+	[_LBASE] = LAYOUT_ortho_5x15(
+		KC_GRV, 			KC_1, 		KC_2, 		KC_3, 		KC_4, 				KC_5, 		KC_VOLD, 	KC_MUTE, 	KC_VOLU, 	KC_6, 		KC_7, 				KC_8, 			KC_9, 			KC_0, 		KC_DEL, 
+		KC_TAB, 			KC_Q, 		KC_W, 		KC_E, 		KC_R, 				KC_T, 		KC_MPRV, 	KC_MPLY, 	KC_MNXT, 	KC_Y, 		KC_U, 				KC_I,			KC_O, 			KC_P, 		KC_BSPC, 
+		LCTL_T(KC_ESC), 	KC_A, 		KC_S, 		KC_D, 		KC_F, 				KC_G, 		XXXXXXX, 	XXXXXXX, 	KC_F15, 	KC_H, 		KC_J, 				KC_K, 			KC_L, 			KC_SCLN, 	RCTL_T(KC_QUOT), 
+		KC_LSFT, 			KC_Z, 		KC_X, 		KC_C, 		KC_V, 				KC_B, 		XXXXXXX, 	XXXXXXX, 	KC_F14, 	KC_N, 		KC_M, 				KC_COMM, 		KC_DOT, 		KC_SLSH, 	SC_SENT, 
+		SGUI_T(KC_MUTE), 	KC_LCTL, 	KC_LALT, 	KC_LGUI, 	LT(_LGAME,KC_SPC), 	KC_SPC, 	KC_SPC, 	XXXXXXX, 	KC_SPC, 	KC_SPC, 	LT(_LSYMB,KC_SPC), 	RCTL(KC_RSFT), 	RCTL(KC_RALT), 	KC_RALT, 	LT(_LARRW,KC_DOWN)
+	),
+	[_LSYMB] = LAYOUT_ortho_5x15(
+		_______, 			_______, 	_______, 	_______, 	_______, 			_______, 	_______, 	_______, 	_______, 	_______, 	_______, 			_______, 		_______, 		_______, 	_______, 
+		KC_TILD, 			KC_EXLM, 	KC_AT, 		KC_HASH, 	KC_DLR, 			KC_PERC, 	_______, 	_______, 	_______, 	KC_CIRC, 	KC_AMPR, 			KC_ASTR, 		KC_LPRN, 		KC_RPRN, 	KC_UNDS, 
+		_______, 			KC_UNDS, 	KC_LPRN, 	KC_LBRC, 	KC_LCBR, 			KC_EQL, 	_______, 	_______, 	_______, 	KC_PLUS, 	KC_RCBR, 			KC_RBRC, 		KC_RPRN, 		KC_COLN, 	KC_DQUO, 
+		_______, 			_______, 	KC_BSLS, 	KC_PIPE, 	KC_GRV, 			_______, 	_______, 	_______, 	_______, 	KC_INS, 	KC_MINS, 			KC_LT, 			KC_GT, 			_______, 	_______, 
+		_______, 			_______, 	_______, 	_______, 	_______, 			_______, 	_______, 	_______, 	_______, 	_______, 	_______, 			KC_HOME, 		KC_PGUP, 		KC_PGDN, 	KC_END
+	),
+	[_LGAME] = LAYOUT_ortho_5x15(
+		_______, 			KC_F1, 		KC_F2, 		KC_F3, 		KC_F4, 				KC_F5, 		KC_F6, 		KC_F7, 		KC_F8, 		KC_F9, 		KC_F10, 			KC_F11, 		KC_F12, 		KC_PSCR, 	KC_SCRL, 
+		_______, 			_______, 	KC_WH_U, 	KC_MS_U, 	KC_WH_D, 			_______, 	_______, 	_______, 	_______, 	_______, 	_______, 			_______, 		_______, 		_______, 	QK_BOOT, 
+		_______, 			_______, 	KC_MS_L, 	KC_MS_D, 	KC_MS_R, 			_______, 	_______, 	_______, 	_______, 	RGB_TOG, 	RGB_MOD, 			RGB_HUI, 		RGB_SAI, 		RGB_VAI, 	RGB_SPI, 
+		_______, 			_______, 	_______, 	KC_BTN2, 	KC_BTN1, 			_______, 	_______, 	_______, 	_______, 	_______, 	RGB_RMOD, 			RGB_HUD, 		RGB_SAD, 		RGB_VAD, 	RGB_SPD, 
+		_______, 			_______, 	_______, 	_______, 	_______, 			_______, 	_______, 	_______, 	_______, 	_______, 	_______, 			_______, 		_______, 		_______, 	_______
+	),
+	[_LARRW] = LAYOUT_ortho_5x15(
+		XXXXXXX, 			XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 			XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 			XXXXXXX, 		XXXXXXX, 		XXXXXXX, 	XXXXXXX, 
+		_______, 			XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 			XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 			XXXXXXX, 		XXXXXXX, 		XXXXXXX, 	XXXXXXX, 
+		_______, 			XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 			XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 			XXXXXXX, 		XXXXXXX, 		XXXXXXX, 	XXXXXXX, 
+		_______, 			XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 			XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 			XXXXXXX, 		KC_UP, 			XXXXXXX, 	XXXXXXX, 
+		_______, 			_______, 	_______, 	_______, 	XXXXXXX, 			XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 	XXXXXXX, 			KC_LEFT, 		KC_DOWN, 		KC_RGHT, 	XXXXXXX
+	)
 };
 
