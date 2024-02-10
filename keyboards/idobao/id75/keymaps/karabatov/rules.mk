2eea1cfa1372ecb7c54b1effa4f70d227c227b38
Yuri Karabatov <yuri@yurikarabatov.com>
2024-02-10 12:19:34 +0000

    Reformat and decrease AVR size

---
+++ keyboards/idobao/id75/keymaps/karabatov/rules.mk
@@ -0,0 +1,12 @@
+# Link time optimization
+LTO_ENABLE = yes
+
+# Debug console
+CONSOLE_ENABLE = no
+
+# Need this for SC_SENT
+SPACE_CADET_ENABLE = yes
+GRAVE_ESC_ENABLE = no 
+MAGIC_ENABLE = no
+MUSIC_ENABLE = no
+AVR_USE_MINIMAL_PRINTF = yes
